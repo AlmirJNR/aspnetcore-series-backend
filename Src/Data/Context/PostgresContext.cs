31309ea9d52a4a0cbe59e0256a11afe21762ce8f
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-09-08 14:24:35 -0300

    refactor: remove unused methods

--- Src/Data/Context/PostgresContext.cs
+++ Src/Data/Context/PostgresContext.cs
@@ -17,21 +17,8 @@ public partial class PostgresContext : DbContext
     public virtual DbSet<Category> Categories { get; set; } = null!;
     public virtual DbSet<Series> Series { get; set; } = null!;
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseNpgsql(
-                "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=password;");
-        }
-    }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(PostgresContext).Assembly);
-
-        OnModelCreatingPartial(modelBuilder);
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
