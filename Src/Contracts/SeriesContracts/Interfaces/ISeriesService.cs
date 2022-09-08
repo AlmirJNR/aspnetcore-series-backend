0c89145ebaba3536639459ce7fda1207f84a7666
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-09-08 14:34:04 -0300

    feat: add 'InsertNewCategory' method on the interface

--- Src/Contracts/SeriesContracts/Interfaces/ISeriesService.cs
+++ Src/Contracts/SeriesContracts/Interfaces/ISeriesService.cs
@@ -8,6 +8,7 @@ public interface ISeriesService
     public Task<Series?> AddNew(CreateSeriesDto createSeriesDto);
     public Task<bool> DeleteAllIds(IEnumerable<Guid> ids);
     public Task<bool> DeleteById(Guid id);
+    public Task<bool> InsertNewCategory(Guid seriesId, Guid categoryId);
     public Task<IEnumerable<ReducedSeriesDto>> GetAllPaginated(bool includeDeleted, int page, int limit);
     public Task<Series?> GetById(Guid id);
     public Task<bool> UpdateById(Guid id, UpdateSeriesDto updateSeriesDto);
