07e4f9f522968f941a1e3fba3acc4f3b1ee920c6
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-09-08 14:57:22 -0300

    refactor: use interfaces instead of classes in the dependecy injection

--- Src/Api/Program.cs
+++ Src/Api/Program.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using Contracts.Categories.Interfaces;
+using Contracts.Repositories.Interfaces;
+using Contracts.SeriesContracts.Interfaces;
 using Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -13,12 +16,12 @@ builder.Services.AddDbContext<PostgresContext>(options =>
                       ?? throw new ArgumentException()));
 
 // Repositories
-builder.Services.AddTransient<CategoriesRepository>();
-builder.Services.AddTransient<SeriesRepository>();
+builder.Services.AddTransient<ICategoryRepository, CategoriesRepository>();
+builder.Services.AddTransient<ISeriesRepository, SeriesRepository>();
 
 // Services
-builder.Services.AddTransient<CategoriesService>();
-builder.Services.AddTransient<SeriesService>();
+builder.Services.AddTransient<ICategoryService, CategoriesService>();
+builder.Services.AddTransient<ISeriesService, SeriesService>();
 
 // Controllers
 builder.Services.AddControllers();
@@ -39,7 +42,11 @@ builder.Services.AddCors(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(config =>
 {
-    config.SwaggerDoc("v1", new OpenApiInfo { Title = "Series API", Version = "v1" });
+    config.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Series API",
+        Version = "v1"
+    });
 
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
